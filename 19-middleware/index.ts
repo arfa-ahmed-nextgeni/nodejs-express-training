aba60e71eb8b0a3f1a0db179c4c4c5d91dbda496
arfa-ahmed-nextgeni <arfa.ahmed@nextgeni.com>
2026-06-29 19:47:58 +0500

    feat: add middleware example and update package.json with new script

---
+++ 19-middleware/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+
+const PORT = 3000;
+
+const app = express();
+
+app.use((_req, _res, next) => {
+  console.log("Application middleware");
+  next();
+});
+
+function auth(_req: express.Request, _res: express.Response, next: express.NextFunction) {
+  console.log("Auth middleware");
+  next();
+}
+
+app.get("/", auth, (_req, res) => {
+  res.send("Home Page");
+});
+
+app.get("/admin", auth, (_req, res) => {
+  res.send("Admin Page");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
