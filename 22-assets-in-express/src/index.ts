5580a91631e9a4c17965a44f2f2879d0c007b162
arfa-ahmed-nextgeni <arfa.ahmed@nextgeni.com>
2026-06-30 19:43:19 +0500

    feat: add welcome route to serve a message in Express

--- 22-assets-in-express/src/index.ts
+++ 22-assets-in-express/src/index.ts
@@ -4,6 +4,10 @@ const PORT = 3000;
 
 const app = express();
 
+app.get("/", (_req, res) => {
+  res.send("Welcome to Express Assets");
+});
+
 app.use(express.static("22-assets-in-express/public"));
 
 app.listen(PORT, () => {
