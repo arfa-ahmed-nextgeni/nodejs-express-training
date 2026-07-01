c2165a99610f207ffc472d8a4968d74cc72a0231
arfa-ahmed-nextgeni <arfa.ahmed@nextgeni.com>
2026-07-01 19:29:52 +0500

    feat: add authorization middleware and routes

---
+++ 27-authorization/src/index.ts
@@ -0,0 +1,45 @@
+import express from "express";
+
+const POST = 3000;
+
+const app = express();
+
+app.use(express.json());
+
+const user = {
+  id: 1,
+  name: "Ahmed",
+  role: "user"
+};
+
+function requireAdmin(_req: express.Request, res: express.Response, next: express.NextFunction) {
+  if (user.role !== "admin") {
+    return res.status(403).json({
+      "message": "Forbidden"
+    });
+  }
+  next();
+}
+
+app.get("/", (_req, res) => {
+  res.json({
+    "message": "Public route"
+  });
+});
+
+app.get("/profile", (_req, res) => {
+  res.json({
+    "message": "Profile data",
+    "user": user
+  });
+});
+
+app.get("/admin", requireAdmin, (_req, res) => {
+  res.json({
+    "message": "Admin data"
+  });
+});
+
+app.listen(POST, () => {
+  console.log(`Server is running on port ${POST}`);
+});
